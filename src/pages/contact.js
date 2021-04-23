c33744428b3a5e0a7480a65222735e54c24390a3
Nazareno Quiroga <naz.quiroga@gmail.com>
2021-04-23 01:06:18 -0300

    Add Contact page with responsive styles

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,9 +1,29 @@
 import React from "react";
+import Form from "../components/Form";
+import Footer from "../components/Footer";
+import Map from "../components/Map";
+import "./Contact.css";
+import ContactAsk from "../components/ContactAsk";
+import contactAskData from "../components/contactAskData";
+
+const contactAskAbout = contactAskData.map((item) => (
+  <ContactAsk key={item.name} feature={item} />
+));
 
 export default function Contact() {
   return (
-    <div className="products">
-      <h1>Contact</h1>
-    </div>
+    <React.Fragment>
+      <div className="contact-container">
+        <div>
+          <h1 className="contact-title">Contact</h1>
+          <h2 className="contact-subtitle">Ask us about</h2>
+          {contactAskAbout}
+        </div>
+        <Form />
+      </div>
+      <h2 className="contact-map-title">Headquarters</h2>
+      <Map />
+      <Footer />
+    </React.Fragment>
   );
 }
