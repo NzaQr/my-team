883c541627f9a0f399307867256c5416d8baefeb
Nazareno Quiroga <naz.quiroga@gmail.com>
2021-04-22 21:13:38 -0300

    Add component with responsive styles

---
+++ src/components/Directors.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import * as AiIcons from "react-icons/ai";
+import "./Director.css";
+
+export default function Directors({
+  director: { img, name, position, description },
+}) {
+  const [flip, setFlip] = useState(false);
+  return (
+    <div className="director">
+      <div
+        className={`flip-card-inner ${flip ? "flip" : ""}`}
+        onClick={() => setFlip(!flip)}
+      >
+        <div className="flip-card-front">
+          <img className="director-img" src={img} alt="director" />
+          <h3 className="director-name">{name}</h3>
+          <p className="director-position">{position}</p>
+        </div>
+        <div className="flip-card-back">
+          <h3 className="director-name-back">{name}</h3>
+          <p className="director-description">{description}</p>
+          <a className="social-icon" href="https://twitter.com">
+            <AiIcons.AiOutlineTwitter />
+          </a>
+          <a className="social-icon" href="https://www.linkedin.com">
+            <AiIcons.AiFillLinkedin />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
