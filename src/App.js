308de5c7edde51caeab0138650f3a2392913767c
Nazareno Quiroga <naz.quiroga@gmail.com>
2021-04-29 16:02:53 -0300

    Fix scroll to top between pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./components/Header";
+import ScrollToTop from "./components/ScrollToTop";
+
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-
 export default function App() {
   return (
     <React.Fragment>
       <Router>
         <Header />
+        <ScrollToTop />;
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/about" component={About} />
